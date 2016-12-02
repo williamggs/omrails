1c2156f5cca587fe51ebc3308b643d603e0f8120
William Geiser <williamggs@gmail.com>
2016-12-02 03:58:42 -0800

    Jquery.ui. downgrade and dashboard

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootstrap', '~> 4.0.0.alpha5'
 gem 'devise'
+gem 'activeadmin', '~> 1.0.0.pre4'
+gem 'inherited_resources', github: 'activeadmin/inherited_resources'
+gem 'jquery-ui-rails', '~> 4.2.1'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require turbolinks
-//= require bootstrap-sprockets
+//= require jquery
+//= require jquery.ui.datepicker
+//= require jquery_ujs
 //= require_tree .
